30981c141236c8d9cf43bbbb88d9f3a694f72456
Omar Bayoumi <obayoumi@getgroup.com>
2025-11-02 16:03:29 +0200

    add main serivces to main page

---
+++ src/app/pages/main/componnts/main-services/main-services.component.ts
@@ -0,0 +1,21 @@
+import { Component, inject } from '@angular/core';
+import { openWhatsApp } from '../../../../shared/funcations/whatapp';
+import { LanguageService } from '../../../../shared/services/language.service';
+import { SERVICES } from '../../../services/data/services';
+import { DecimalPipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { TranslatePipe } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-main-services',
+  imports: [TranslatePipe, DecimalPipe, RouterLink],
+  templateUrl: './main-services.component.html',
+  styleUrl: './main-services.component.scss',
+})
+export class MainServicesComponent {
+  langService = inject(LanguageService);
+  currentLang = this.langService.currentLangSignal;
+  // WhatsApp
+  openWhatsApp = openWhatsApp;
+  SERVICES = SERVICES;
+}
